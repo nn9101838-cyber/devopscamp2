816ffb7adbc6d36344a4708e9d91159fd599e91c
DevOps Student <student@example.com>
2026-03-25 14:46:01 +0530

    feat: add addition calculator

--- app.c
+++ app.c
@@ -2,9 +2,15 @@
 
 int main() {
     char name[100];
+    int num1, num2;
     printf("Welcome to DevOps C App\n");
     printf("Enter your name: ");
     scanf("%99s", name);
     printf("Hello, %s!\n", name);
+
+    printf("Enter two integers to add: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Sum: %d\n", num1 + num2);
+
     return 0;
 }
