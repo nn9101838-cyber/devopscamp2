e92c5018dce06baf54b9577b4eef4416a6ba691d
Nandi <nandi@example.com>
2026-03-25 15:38:27 +0530

    Add login feature

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ print("Welcome to DevOps Camp 2 Project!")
 def calculate_square(n):
     return n * n
 
+def login(username, password):
+    if username == "admin" and password == "secret":
+        return True
+    return False
+
 if __name__ == "__main__":
     print(f"Square of 5 is {calculate_square(5)}")
+    print(f"Login result: {login('admin', 'secret')}")
