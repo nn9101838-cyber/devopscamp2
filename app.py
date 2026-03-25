63f109e228c2a1ed3be136011c0a4e189a226f0e
Nandi <nandi@example.com>
2026-03-25 15:40:25 +0530

    Add notification feature

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ print("Welcome to DevOps Camp 2 Project!")
 def calculate_square(n):
     return n * n
 
+def send_notification(message):
+    print(f"Notification Sent: {message}")
+    return True
+
 if __name__ == "__main__":
     print(f"Square of 5 is {calculate_square(5)}")
+    send_notification("Welcome to DevOps!")
